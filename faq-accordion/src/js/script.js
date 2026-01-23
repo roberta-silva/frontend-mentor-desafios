9bebaddd60871f6eeeab72e874d39b0607e88189
Roberta Silva <robertadasilva.contato@gmail.com>
2026-01-23 18:22:23 -0300

    Ajusta layout para maior fidelidade ao design

--- faq-accordion/src/js/script.js
+++ faq-accordion/src/js/script.js
@@ -10,7 +10,7 @@ function initAccordion() {
       this.classList.toggle(activeClass);
       this.nextElementSibling.classList.toggle(activeClass);
     }
-
+    
     accordionList.forEach((item) => {
       item.addEventListener("click", activeAccordion);
     });
